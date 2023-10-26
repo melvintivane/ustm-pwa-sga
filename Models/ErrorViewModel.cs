81edf2b2cf21bd5f0a58dcd18bb760319f953f30
melvintivane <melvinshuster47@gmail.com>
2023-10-26 20:37:17 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace pwa_trabalho_sga.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

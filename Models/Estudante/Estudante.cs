cb5c5e40908a2daf7b77df5c946ff81468002308
melvintivane <melvinshuster47@gmail.com>
2023-10-30 07:10:04 +0200

    Update Student class definition to reflect new fields and annotations

--- Models/Estudante/Estudante.cs
+++ Models/Estudante/Estudante.cs
@@ -1,12 +1,18 @@
-namespace pwa_trabalho_sga.Models.Estudante
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pwa_trabalho_sga.Models;
+
+[Table("estudante")]
+public class Estudante
 {
-    public class Estudante {
-        public Guid Id { get; set; }
-        public int NrEstudante { get; set; }
-        public string Nome { get; set; }
-        public string Turma { get; set; }
-        public string Curso { get; set; }
-        public string Notas { get; set; }
-        public string Exames { get; set; }
-    }
+    [Key]
+    public Guid Id { get; set; }
+    public UserRole Role = UserRole.Estudante;
+    public int? NrEstudante { get; set; }
+    public string? Nome { get; set; }
+    public string? Turma { get; set; }
+    public string? Curso { get; set; }
+    public string? Notas { get; set; }
+    public string? Exames { get; set; }
 }

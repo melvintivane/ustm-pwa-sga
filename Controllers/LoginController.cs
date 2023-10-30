98f0d0295794cdf876df9d783a7e9ee4f3b7f9dd
melvintivane <melvinshuster47@gmail.com>
2023-10-30 07:14:38 +0200

    Refactor LoginController for better code organization

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,25 +2,26 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using pwa_trabalho_sga.Models;
 
-namespace pwa_trabalho_sga.Controllers;
-
-public class LoginController : Controller
+namespace pwa_trabalho_sga.Controllers
 {
-    private readonly ILogger<LoginController> _logger;
-
-    public LoginController(ILogger<LoginController> logger)
+    public class LoginController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<LoginController> _logger;
 
-    public IActionResult Login()
-    {
-        return View();
-    }
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }

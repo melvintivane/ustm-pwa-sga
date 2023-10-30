9dedfed46e2648ce0158ae79b3e17cd8ab497b56
melvintivane <melvinshuster47@gmail.com>
2023-10-30 07:02:42 +0200

    Create ApplicationDbContext.cs

---
+++ Models/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace pwa_trabalho_sga.Models
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Estudante> Estudantes { get; set; }
+        // public DbSet<Professor> Professores { get; set; }
+        // public DbSet<Admin> Admins { get; set; }
+    }
+}

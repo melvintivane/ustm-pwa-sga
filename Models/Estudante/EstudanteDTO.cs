b7d590b905a2d6cc5a12db0857a31659403d406b
melvintivane <melvinshuster47@gmail.com>
2023-11-01 15:58:52 +0200

    Create EstudanteDTO.cs

---
+++ Models/Estudante/EstudanteDTO.cs
@@ -0,0 +1,12 @@
+namespace pwa_trabalho_sga.Models;
+
+public class EstudanteDTO
+{
+    public int? NrEstudante { get; set; }
+    public string? Password { get; set; }
+    public string? Nome { get; set; }
+    public string? Turma { get; set; }
+    public string? Curso { get; set; }
+    public string? Notas { get; set; }
+    public string? Exames { get; set; }
+}

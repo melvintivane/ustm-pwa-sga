5e6200a34fc67896f3caa5054132602483750bd3
melvintivane <melvinshuster47@gmail.com>
2023-11-07 15:33:26 +0200

    Update Estudante DB Table via Migration

---
+++ backend/Migrations/20231106140347_UpdateEstudante.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pwa_trabalho_sga.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEstudante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "estudantes",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "estudantes");
+        }
+    }
+}

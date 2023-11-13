33073ea6b68e6c5d849f705b490183d713a68157
melvintivane <melvinshuster47@gmail.com>
2023-11-13 07:55:39 +0200

    refactor: Removes Turma, Curso and Exames properties from StudentDTO

--- backend/Models/Estudante/EstudanteDTO.cs
+++ backend/Models/Estudante/EstudanteDTO.cs
@@ -5,8 +5,5 @@ public class EstudanteDTO
     public int? NrEstudante { get; set; }
     public string? Password { get; set; }
     public string? Nome { get; set; }
-    public string? Turma { get; set; }
-    public string? Curso { get; set; }
     public string? Notas { get; set; }
-    public string? Exames { get; set; }
 }

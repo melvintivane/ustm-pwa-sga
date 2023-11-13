c5ef417741476b5b10a366b6bc75d8816afa0ee4
melvintivane <melvinshuster47@gmail.com>
2023-11-13 07:50:43 +0200

    Add AddNotas and UpdateNotas methods to INotaRepository

--- backend/Models/Estudante/Nota/INotaRepository.cs
+++ backend/Models/Estudante/Nota/INotaRepository.cs
@@ -1,8 +1,8 @@
-
-
 namespace pwa_trabalho_sga.Models;
 
 public interface INotaRepository
 {
+    Task<Nota?> AddNotas(Guid estudante, Guid disciplina, Nota notas);
+    Task<Nota?> UpdateNotas(Guid estudante, Guid disciplina, Nota notas);
     Task<List<Nota>?> GetNotas();
 }

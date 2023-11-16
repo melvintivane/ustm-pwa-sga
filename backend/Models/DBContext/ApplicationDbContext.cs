1968ee38b1359b61ac8e48cafe644408fba31ce0
melvintivane <melvinshuster47@gmail.com>
2023-11-16 09:21:17 +0200

    feat: Add model entities in ApplicationDbContext

--- backend/Models/DBContext/ApplicationDbContext.cs
+++ backend/Models/DBContext/ApplicationDbContext.cs
@@ -9,7 +9,15 @@ namespace pwa_trabalho_sga.Models
 
         }
 
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
         public DbSet<Estudante> Estudantes { get; set; }
+        public DbSet<Exame> Exames { get; set; }
+        public DbSet<Inscricao> Inscricoes { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+        public DbSet<Nota> Notas { get; set; }
+        public DbSet<Turma_Disciplina> Turma_Disciplinas { get; set; }
+        public DbSet<Turma> Turmas { get; set; }
         // public DbSet<Professor> Professores { get; set; }
         // public DbSet<Admin> Admins { get; set; }
     }

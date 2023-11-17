af8e997dcbde9ac754ed5db9ebada95ebc732bc2
melvintivane <melvinshuster47@gmail.com>
2023-11-17 10:07:37 +0200

    refactor: Update Estudante model properties

--- backend/Models/Estudante/Estudante.cs
+++ backend/Models/Estudante/Estudante.cs
@@ -3,17 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace pwa_trabalho_sga.Models;
 
-[Table("estudante")]
+[Table("estudantes")]
 public class Estudante
 {
     [Key]
     public Guid Id { get; set; }
-    public UserRole Role = UserRole.Estudante;
+    public string? Role { get; set; } 
     public int? NrEstudante { get; set; }
     public string? Password { get; set; }
     public string? Nome { get; set; }
-    public string? Turma { get; set; }
-    public string? Curso { get; set; }
-    public string? Notas { get; set; }
-    public string? Exames { get; set; }
+    public string? NumeroBI { get; set; }
 }

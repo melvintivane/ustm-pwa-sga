9fb026e1291805e7d86a8c54a83042cfdaa0ce94
melvintivane <melvinshuster47@gmail.com>
2024-04-07 17:13:24 +0200

    chore: create Estudante component

---
+++ frontend/src/pages/Estudante/Estudante.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Table from "../../components/Table/Table"
+import "./Estudante.css";
+
+const Estudante = () => {
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
+};
+
+export default Estudante;
